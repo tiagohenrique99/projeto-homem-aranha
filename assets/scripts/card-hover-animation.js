76f19619f22f119dbc9dfd6bd03444d985c0617b
Tiago Henrique da silva Barbosa <122296117+tiagohenrique99@users.noreply.github.com>
2023-03-11 00:06:48 -0300

    1

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -11,7 +11,7 @@ function handleMouseLeave() {
 function addEventListenerstocards() {
     const cardElements = document.getElementsByClassName('s-card');
     
-    for(let index = 0; index < cardElements.length; index++) {
+    for (let index = 0; index < cardElements.length; index++) {
         const card = cardElements[index];
         card.addEventListener('mouseenter', handleMouseEnter);
         card.addEventListener('mouseleave', handleMouseLeave);
